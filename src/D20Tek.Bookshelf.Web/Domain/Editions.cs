05ff9ff01ae727da879e1c653094ccec97ab88ec
Pedro Silva <DarthPedro@gmail.com>
2025-04-07 16:09:41 -0700

    Make the final edition EndYear auto calculate.

--- src/D20Tek.Bookshelf.Web/Domain/Editions.cs
+++ src/D20Tek.Bookshelf.Web/Domain/Editions.cs
@@ -10,7 +10,7 @@ public sealed class Editions
         new("3ed", "Dungeons & Dragons 3rd Edition","DnD3E", 2000, 2003),
         new("35ed", "Dungeons & Dragons v.3.5","DnD35E", 2003, 2008),
         new("4ed", "Dungeons & Dragons 4th Edition","DnD4E", 2008, 2014),
-        new("5ed2024", "Dungeons & Dragons 2024", "DnD2024", 2024, 2025)
+        new("5ed2024", "Dungeons & Dragons 2024", "DnD2024", 2024, DateTime.Now.Year)
     ];
 
     public static DndEdition GetEdition(string code) => _editions.First(x => x.Code == code);
