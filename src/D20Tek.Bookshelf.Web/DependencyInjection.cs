f5c6c031b2ae5c9d521713661e23337441d33f32
Pedro Silva <DarthPedro@gmail.com>
2025-04-09 15:08:56 -0700

    Refactored Program.cs file to use DependencyInjection extenion methods.

---
+++ src/D20Tek.Bookshelf.Web/DependencyInjection.cs
@@ -0,0 +1,37 @@
+﻿using D20Tek.Bookshelf.Web.Features.Books;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace D20Tek.Bookshelf.Web;
+
+public static class DependencyInjection
+{
+    private const string _serviceTestSleepDelay = "TestSleepDelay";
+
+    public static WebAssemblyHostBuilder AddBlazorRoot(this WebAssemblyHostBuilder builder)
+    {
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        return builder;
+    }
+
+    public static WebAssemblyHostBuilder AddPresentationServices(this WebAssemblyHostBuilder builder) =>
+        builder.AddHttpClient()
+               .AddServices();
+
+    private static WebAssemblyHostBuilder AddHttpClient(this WebAssemblyHostBuilder builder)
+    {
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        Constants.ServiceSleepDelay = builder.Configuration.GetValue<int>(_serviceTestSleepDelay, 0);
+
+        return builder;
+    }
+
+    private static WebAssemblyHostBuilder AddServices(this WebAssemblyHostBuilder builder)
+    {
+        builder.Services.AddScoped<IBooksService, BooksService>();
+
+        return builder;
+    }
+}
--- src/D20Tek.Bookshelf.Web/Program.cs
+++ src/D20Tek.Bookshelf.Web/Program.cs
@@ -1,19 +1,13 @@
-global using D20Tek.Functional;
 global using D20Tek.Bookshelf.Web;
 global using D20Tek.Bookshelf.Web.Common;
 global using D20Tek.Bookshelf.Web.Domain;
+global using D20Tek.Functional;
 global using Microsoft.AspNetCore.Components;
 
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using D20Tek.Bookshelf.Web.Features.Books;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<IBooksService, BooksService>();
-Constants.ServiceSleepDelay = builder.Configuration.GetValue<int>("TestSleepDelay", 0);
+var builder = WebAssemblyHostBuilder.CreateDefault(args)
+                                    .AddBlazorRoot()
+                                    .AddPresentationServices();
 
 await builder.Build().RunAsync();
