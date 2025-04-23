b2643383c9dd33df4c0e324c9dbf1cc20a9eccb3
Pedro Silva <DarthPedro@gmail.com>
2025-04-23 15:47:07 -0700

    Added basic auth code to application startup and dependency injection. Added AuthenticationService to main index.html. Added CustomAuthorizationMessageHandler and Authentication.razor component for use in the auth process.

--- src/D20Tek.Bookshelf.Web/DependencyInjection.cs
+++ src/D20Tek.Bookshelf.Web/DependencyInjection.cs
@@ -7,6 +7,10 @@ namespace D20Tek.Bookshelf.Web;
 
 public static class DependencyInjection
 {
+    private const string _authConfigSection = "Auth0";
+    private const string _authResponseType = "code";
+    private const string _authAudience = "audience";
+    private const string _authAudienceConfig = "Auth0:Audience";
     private const string _serviceTestSleepDelay = "TestSleepDelay";
 
     public static WebAssemblyHostBuilder AddBlazorRoot(this WebAssemblyHostBuilder builder)
@@ -19,6 +23,7 @@ public static class DependencyInjection
 
     public static WebAssemblyHostBuilder AddPresentationServices(this WebAssemblyHostBuilder builder) =>
         builder.AddHttpClient()
+               .AddOicdAuth()
                .AddServices();
 
     private static WebAssemblyHostBuilder AddHttpClient(this WebAssemblyHostBuilder builder)
@@ -28,6 +33,21 @@ public static class DependencyInjection
 
         return builder;
     }
+
+    private static WebAssemblyHostBuilder AddOicdAuth(this WebAssemblyHostBuilder builder)
+    {
+        builder.Services.AddOidcAuthentication(options =>
+        {
+            builder.Configuration.Bind(_authConfigSection, options.ProviderOptions);
+            options.ProviderOptions.ResponseType = _authResponseType;
+            options.ProviderOptions.AdditionalProviderParameters.Add(_authAudience, builder.GetAudienceConfig());
+        });
+
+        return builder;
+    }
+
+    private static string GetAudienceConfig(this WebAssemblyHostBuilder builder) =>
+        builder.Configuration[_authAudienceConfig] ?? string.Empty;
 
     private static WebAssemblyHostBuilder AddServices(this WebAssemblyHostBuilder builder)
     {
